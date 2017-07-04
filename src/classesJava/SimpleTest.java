5be765f48d3a5aac01a2791f9a3b3391e11affc4
Vasiliy Kizimov <v.kizimov@carfix.ru>
2017-07-05 00:29:54 +0300

    commithouse

--- src/classesJava/SimpleTest.java
+++ src/classesJava/SimpleTest.java
@@ -5,13 +5,13 @@ import java.sql.SQLException;
 public class SimpleTest {
 
     public static void main(String[] args) throws SQLException, DBSystemException, ClassNotFoundException, NotUniqueUserLoginException, NotUniqueUserEmaiException {
-        ConnectionFactoryFactory.setType(ConnectionFactoryFactory.FactoryType.DBCP);
+//        ConnectionFactoryFactory.setType(ConnectionFactoryFactory.FactoryType.DBCP);
         UserDao dao = new UserDaoJdbs();
 //        List<User> listUsers = dao.selectAll();
 //        dao.deleteById(7);
        User user = new User();
        user.setId(20);user.setLogin("ffFD");user.setEmail("fDffF");user.setUsercol("Dcoll");
-       dao.insert(user);
+      dao.insert(user);
 //        for (User us:listUsers) {
 //            System.out.println(us);
 //        }
